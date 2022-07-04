911646d75ae1292b6502de9ea9234747423d2e61
RealF0X <107392879+RealF0X@users.noreply.github.com>
2022-07-04 02:20:56 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,33 +1,27 @@
-import imp
-
-
 import os
 os.system("cls")
 import random
 
+playAgain = "Y"
+while playAgain == "Y" :
+    superHeroes = ["Ironman","Thor","DeadPool","Antman","Avengers","Hulk","BlackPanther","SpiderMan","Venom","Batman","Superman","WonderWoman","DoctorStrange"]
+    gamecontinue = "Y"
+    hangMan = list(random.choice(superHeroes).lower())
+    guessHangman = ["_"]*len(hangMan)
+    print("\nYou Have to Try to Guess  Name of a SuperHero\n")
+    print(guessHangman)
 
-superHeroes = ["Ironman","Thor","DeadPool","Antman","Avengers","Hulk","BlackPanther","SpiderMan","Venom","Batman","Superman","WonderWoman","DoctorStrange"]
-print(superHeroes)
-gamecontinue = "Y"
-hangMan = list(random.choice(superHeroes).lower())
-print(hangMan)
-guessHangman = ["_"]*len(hangMan)
-print("\nYou Have to Try to Guess  Name of a SuperHero\n")
-print(guessHangman)
 
-while guessHangman.count("_") > 0 :
-    
-    userGuess = input("Pls enter a Letter : ")
-    for i in range(len(hangMan)) :
-        if userGuess == hangMan[i] :
-            guessHangman[i] = userGuess
-            print(guessHangman)
+    while guessHangman.count("_") > 0 :
         
-        elif userGuess != hangMan[i] :
-            print("\nThe Letter You Typed isnn\'t in The Word Try Again\n")
-            break
+        userGuess = input("Pls enter a Letter : ")
+        for i in range(len(hangMan)) :
+            if userGuess == hangMan[i] :
+                guessHangman[i] = userGuess
+                print(guessHangman)
+                
+    playAgain = input("Do You Want To Play Again?(Y/N) ").upper()        
         
-
 
 
 
