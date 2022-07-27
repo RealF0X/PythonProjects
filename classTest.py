d3d738df933acc1bdfc85aa56edb3c43bfa76273
RealF0X <107392879+RealF0X@users.noreply.github.com>
2022-07-27 11:06:42 -0700

    Add files via upload

---
+++ classTest.py
@@ -0,0 +1,13 @@
+class Addition:
+    def __init__(self,number1,number2):
+        self.number1 = number1
+        self.number2 = number2
+        
+    
+    def sumOfTwo():
+        return(userNumber1 + userNumber2)
+
+userNumber1 = int(input('Pls Enter a Number : '))
+userNumber2 = int(input('Pls Enter another Number : '))
+
+print(Addition.sumOfTwo())
